fa9561732f2dce13806ad894c0f0ba48ef786eaa
Billy <longnd1@bravebits.vn>
2024-05-26 00:45:36 +0700

    Update env

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,10 +1,10 @@
 import mongoose, { ConnectOptions, Mongoose } from "mongoose";
 import { Db } from "mongodb";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const MONGODB_URI: string = process.env.MONGODB_URI || "";
+
+// Check if the database is local
+const isLocalDtb = MONGODB_URI?.includes("localhost");
 
 if (!MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
@@ -19,13 +19,16 @@ async function dbConnect(): Promise<Db> {
   }
 
   if (!cached.promise) {
-    const opts: ConnectOptions = {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      bufferCommands: false,
-    };
+    // Set connection options based on environment (local or remote)
+    const options: ConnectOptions = !isLocalDtb
+      ? {
+          user: process.env.DB_USER,
+          pass: process.env.DB_PASSWORD,
+          dbName: process.env.DB_NAME,
+        }
+      : {};
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
       return mongoose;
     });
   }
