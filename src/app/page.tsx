9beb140cb8fcbfd4a0b6c7a90f836936bf77b4c9
Billy <longnd1@bravebits.vn>
2024-05-26 00:51:48 +0700

    Add dbConnect to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import ContentDetail from "@/components/ContentDetail";
 import FooterDetail from "@/components/FooterDetail";
-import HeaderDetail from "@/components/HeaderDetail";
+import dbConnect from "@/lib/dbConnect";
 import { Flex } from "antd";
+
+// Ensure database connection when the application starts
+dbConnect();
 
 export default function Home() {
   return (
